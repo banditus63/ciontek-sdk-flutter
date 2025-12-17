ab3472d970851e4337f64235f803f52fbbbb7f8d
Donrik <36836610+Donrik@users.noreply.github.com>
2025-12-17 13:01:00 +0300

    Add support for qr

--- android/src/main/kotlin/com/imgok/ciontek/models/PrintLine.kt
+++ android/src/main/kotlin/com/imgok/ciontek/models/PrintLine.kt
@@ -1,4 +1,5 @@
 package com.imgok.ciontek
+import com.imgok.ciontek.models.PrintLine
 
 class PrintLine(
     val text: String,
@@ -12,16 +13,18 @@ class PrintLine(
 ){
     companion object {
         fun fromMap(map: Map<String, Any>): PrintLine {
-            return PrintLine(
-                text = map["text"] as String,
-                fontSize = (map["fontSize"] as? Double ?: 24.0).toFloat(),
-                textGray = map["textGray"] as Int,
-                bold = map["bold"] as Boolean,
-                underline = map["underline"] as Boolean,
-                type = map["type"] as String,
-                alignment = (map["alignment"] as? Int),
-                image = map["image"] as? ByteArray,
-            )
+    return PrintLine(
+        text = map["text"] as? String ?: "",
+        // !!! THIS IS THE IMPORTANT PART !!!
+        // Flutter sends numbers as Doubles, so we cast to Double then to Float
+        fontSize = (map["fontSize"] as? Double ?: 24.0).toFloat(), 
+        textGray = map["textGray"] as? Int ?: 3,
+        bold = map["bold"] as? Boolean ?: false,
+        underline = map["underline"] as? Boolean ?: false,
+        type = map["type"] as? String ?: "TEXT",
+        alignment = map["alignment"] as? Int,
+        image = map["image"] as? ByteArray
+             )
         }
     }
 }
