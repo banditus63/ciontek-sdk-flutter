90ff72e9938d113d20ff71bbcc2c4af9711aa8d5
Donrik <36836610+Donrik@users.noreply.github.com>
2025-12-29 21:51:26 +0300

    Fine tuning

--- android/src/main/kotlin/com/imgok/ciontek/CiontekPrintHelper.kt
+++ android/src/main/kotlin/com/imgok/ciontek/CiontekPrintHelper.kt
@@ -51,7 +51,7 @@ object CiontekPrintHelper {
                 val multiplier: Byte
 
                 if (line.fontSize >= 32f) {
-                fontType = 3f.toByte()
+                fontType = 16.toByte()
                 multiplier = 3.toByte()
                 } else {
                  fontType = 24.toByte()
